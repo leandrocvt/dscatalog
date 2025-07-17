31f2f0a05f2d15d1e905d89a21c4074ccdc9fcef
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2025-07-17 01:06:38 -0300

    Feat: Password Recover: save new password with token valid

--- backend/src/main/java/com/devsuperior/dscatalog/resources/AuthResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/AuthResource.java
@@ -1,14 +1,12 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.lib.dto.EmailDTO;
+import com.devsuperior.dscatalog.lib.dto.NewPasswordDTO;
 import com.devsuperior.dscatalog.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,12 @@ public class AuthResource {
     @PostMapping(value = "/recover-token")
     public ResponseEntity<Void> createRecoverToken(@Valid @RequestBody EmailDTO body){
         authService.createRecoverToken(body);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/new-password")
+    public ResponseEntity<Void> saveNewPassword(@Valid @RequestBody NewPasswordDTO body){
+        authService.saveNewPassword(body);
         return ResponseEntity.noContent().build();
     }
 
--- backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.lib.dto.EmailDTO;
+import com.devsuperior.dscatalog.lib.dto.NewPasswordDTO;
 import com.devsuperior.dscatalog.lib.entities.PasswordRecover;
 import com.devsuperior.dscatalog.lib.entities.User;
 import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
@@ -8,9 +9,12 @@ import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -23,6 +27,8 @@ public class AuthService {
 
     @Value("${email.password-recover.uri}")
     private String recoverUri;
+
+    private final PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
 
@@ -48,5 +54,18 @@ public class AuthService {
         String text = "Acesse o link para definir uma nova senha\n\n" + recoverUri + token + ". Validade de " + tokenMinutes + " minutos";
 
         emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+    }
+
+    @Transactional
+    public void saveNewPassword(NewPasswordDTO body) {
+
+        List<PasswordRecover> result = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+        if (result.isEmpty()){
+            throw new ResourceNotFoundException("Token inválido!");
+        }
+
+        User user = userRepository.findByEmail(result.get(0).getEmail());
+        user.setPassword(passwordEncoder.encode(body.getPassword()));
+        userRepository.save(user);
     }
 }
